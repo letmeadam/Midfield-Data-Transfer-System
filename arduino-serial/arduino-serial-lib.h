c137ed0a82c4be58e928934bc9e3ce398312f34d
Adam <letmeadam@gmail.com>
2017-03-17 14:04:09 -0700

    Re-Initialized Directory
    Fixed “arduino-serial” to be a valid directory

---
+++ arduino-serial/arduino-serial-lib.h
@@ -0,0 +1,24 @@
+//
+// arduino-serial-lib -- simple library for reading/writing serial ports
+//
+// 2006-2013, Tod E. Kurt, http://todbot.com/blog/
+//
+
+
+#ifndef __ARDUINO_SERIAL_LIB_H__
+#define __ARDUINO_SERIAL_LIB_H__
+
+#include <stdint.h>   // Standard types 
+#include <stdio.h>
+
+int serialport_init(const char* serialport, int baud);
+int serialport_close(int fd);
+int serialport_writebyte( int fd, uint8_t b);
+int serialport_write(int fd, const char* str);
+int serialport_read_until(int fd, char* buf, char until, int buf_max,int timeout);
+int serialport_read_byte(int fd, uint8_t *byte, int timeout);
+int serialport_read_byte_file(FILE *f, uint8_t *byte, int timeout);
+int serialport_flush(int fd);
+
+#endif
+
